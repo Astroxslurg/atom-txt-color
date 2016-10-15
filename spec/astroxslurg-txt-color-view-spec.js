8246b547c1f18b0d88536b08fa81cef21fd8ccf1
astroxslurg <kkrorkk@gmail.com>
2016-10-15 22:25:52 +0200

    initial commit
    created project with atom package generator

---
+++ spec/astroxslurg-txt-color-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AstroxslurgTxtColorView from '../lib/astroxslurg-txt-color-view';
+
+describe('AstroxslurgTxtColorView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
